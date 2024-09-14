26dc11d8d6b0063cd18bd534c0e36d27905164aa
Manish <mdashsharma95@gmail.com>
2024-09-14 17:46:26 +0530

    fix: Configuration for client

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,8 +1,11 @@
-function App() {
+import React from 'react';
 
+const App: React.FC = () => {
   return (
-    <h1 className="text-center text-4xl text-bold text-blue-500">Task Forge</h1>
-  )
-}
+    <h1 className="text-center text-4xl font-bold text-blue-500">
+      Task Forge
+    </h1>
+  );
+};
 
-export default App
+export default App;
