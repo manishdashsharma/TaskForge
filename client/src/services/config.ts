0ce61e9ecc9c5315e289dbc5abdead35537b2e81
Manish <mdashsharma95@gmail.com>
2024-09-15 13:53:11 +0530

    feat: Used server url from env file in clinet

--- client/src/services/config.ts
+++ client/src/services/config.ts
@@ -1,7 +1,10 @@
 import axios, { AxiosInstance } from 'axios';
 
+// Ensure that the environment variable is defined
+const baseURL = import.meta.env.VITE_SERVER_URL as string;
+
 const servicesAxiosInstance: AxiosInstance = axios.create({
-    baseURL: 'http://localhost:5000'
+    baseURL: baseURL
 });
 
 export {
