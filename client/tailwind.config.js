0ee3e475fd629b1a4a891fca9d870fa344e185b6
Fagbayibo <fagbayibooluwasegun@gmail.com>
2024-09-24 00:13:18 +0100

    Style: Update Tailwindconfig with color and fonts variables

--- client/tailwind.config.js
+++ client/tailwind.config.js
@@ -2,7 +2,13 @@
 export default {
   content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        'forge-darkGreen' : '#003E35',
+        'forge-lightGreen' : '#66D899',
+        'forge-midGreen' : '#1DBA6B'
+      }
+    },
   },
   plugins: [],
 }
