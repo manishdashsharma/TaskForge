b47cde217826a1eb4a7c445e00289855e3459604
Manish <mdashsharma95@gmail.com>
2024-10-07 23:50:41 +0530

    feat: Discord Webwhook config to send api notification

---
+++ server/src/util/DiscordWebhookTransport.ts
@@ -0,0 +1,42 @@
+import axios from 'axios';
+import TransportStream from 'winston-transport';
+
+interface DiscordWebhookTransportOptions {
+    webhookUrl: string;
+    level?: string;
+}
+
+interface LogInfo {
+    level: string;
+    message: string;
+    timestamp?: string;
+    meta?: Record<string, unknown>;
+}
+
+export class DiscordWebhookTransport extends TransportStream {
+    private readonly webhookUrl: string;
+
+    constructor(opts: DiscordWebhookTransportOptions) {
+        super(opts);
+        this.webhookUrl = opts.webhookUrl;
+        this.level = opts.level || 'info';
+    }
+
+    log(info: LogInfo, callback: () => void): void {
+        const { level, message, timestamp, meta = {} } = info;
+        const logTimestamp = timestamp || new Date().toISOString();
+
+        const logMessage = {
+            username: 'Logger Bot',
+            content: `**Level**: ${level.toUpperCase()}\n**Timestamp**: ${logTimestamp}\n**Message**: ${message}\n**Meta**: \`\`\`json\n${JSON.stringify(meta, null, 2)}\`\`\``
+        };
+
+        axios.post(this.webhookUrl, logMessage)
+            .then(() => {
+                callback();
+            })
+            .catch(() => {
+                callback();
+            });
+    }
+}
--- server/src/util/logger.ts
+++ server/src/util/logger.ts
@@ -8,6 +8,8 @@ import path from 'path'
 import { red, blue, yellow, green, magenta } from 'colorette'
 import * as sourceMapSupport from 'source-map-support'
 import { MongoDBTransportInstance } from 'winston-mongodb'
+import { DiscordWebhookTransport } from './DiscordWebhookTransport';
+
 
 // Linking Trace Support
 sourceMapSupport.install()
@@ -113,9 +115,18 @@ const MongodbTransport = (): Array<MongoDBTransportInstance> => {
     ]
 }
 
+const DiscordTransport = (): Array<DiscordWebhookTransport> => {
+    return [
+        new DiscordWebhookTransport({
+            webhookUrl: 'https://discord.com/api/webhooks/1292154448956755978/qJQf8B8ivtBepRjqyABoJDimmksAGM2x2la1SC5TcznlzoPAeQUf-Nf98isATlsJdG9t',
+            level: 'info' // Set the desired log level
+        })
+    ];
+};
+
 export default createLogger({
     defaultMeta: {
         meta: {}
     },
-    transports: [...FileTransport(), ...MongodbTransport(), ...consoleTransport()]
+    transports: [...FileTransport(), ...MongodbTransport(), ...consoleTransport(),...DiscordTransport() ]
 })
