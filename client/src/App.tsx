5d934dafa835424f55fc1f33ff4d2eb650349965
Fagbayibo <fagbayibooluwasegun@gmail.com>
2024-10-13 20:06:24 +0100

    Style: Added SignIn pages, profile pages and routes setup

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -3,6 +3,7 @@ import { Routes, Route, Outlet } from 'react-router-dom';
 import ServerHealthStatus from './Pages/ServerHealthStatus/ServerHealthStatus';
 import Welcome from './Pages/Welcome';
 import SignUp from './Pages/Auth/SignUp/SignUp';
+import SignIn from './Pages/Auth/SignIn/SignIn';
 
 
 const App: React.FC = () => {
@@ -13,6 +14,7 @@ const App: React.FC = () => {
       {/* App Authentication Parent Route */}
       <Route path="/app" element={<Outlet />}>
         <Route path="sign-up" element={<SignUp />} />
+        <Route path = "sign-in" element={<SignIn/>} />
       </Route>
     </Routes>
   );
---
+++ client/src/Pages/ProfilePage/ProfilePage.tsx
@@ -0,0 +1,15 @@
+import React from 'react'
+
+interface Props {
+    
+}
+
+const ProfilePage = (props: Props) => {
+    return (
+        <div>
+            
+        </div>
+    )
+}
+
+export default ProfilePage
